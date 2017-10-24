3cf03c09dce4f5f6225eb213e6d6615358466472
vuong <vuongduongbuh@gmail.com>
2017-10-24 14:14:40 +0700

    init project

---
+++ src/components/feed/feed.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="pl-header pl-header--feed" show.bind="isShowHeader">
+      <div>
+          <img class="pl-icon pl-icon--user" src="../../assets/img/icon-user.png">
+          <button type="button" class="btn btn-primary" click.trigger="search()">
+                  <span class="glyphicon glyphicon-search"></span> 
+                  Search
+              </button>
+      </div>
+      <button type="button" class="btn btn-primary" click.delegate="isInputOnFocus = true">Compose</button>
+  </div>
+</template>
+
+<!--<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>-->
+
+<style lang="scss" scoped>
+  @import "./feed.scss";
+</style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,46 @@
+<template> 
+    <div class="pl-container">
+        <div class="pl-header">
+            <button type="button" class="btn btn-primary">Login</button>
+        </div>
+        <div class="pl-home">
+            <div class="pl-home__title">
+                <span>ankker </span> - your personal log {{msg}}
+            </div>
+
+            <div class="pl-home__feed">
+                Hook what matters to you
+
+                <div class="pl-home__feed--tag">
+                    <span>masterthechaos</span>
+                </div>
+
+                <div class="pl-home__feed--time">
+                    05:03AM - 01 Mar 2017
+                </div>
+            </div>
+
+            <div class="pl-home__feed">
+                Hook what matters to you
+
+                <div class="pl-home__feed--tag">
+                    <span>masterthechaos</span>
+                </div>
+
+                <div class="pl-home__feed--time">
+                    05:03AM - 01 Mar 2017
+                </div>
+            </div>
+
+            <div class="pl-home__buttons">
+                <button type="button" class="btn btn-default">Learn More</button>
+
+                <button type="button" class="btn btn-primary">Sign Up</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss"> 
+    @import "./home.scss";
+</style>
